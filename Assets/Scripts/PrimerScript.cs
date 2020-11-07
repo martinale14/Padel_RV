635159b43cde7d67bfd7efd251322e99c3dcb095
JuanPRivera <68454373+JuanPRivera@users.noreply.github.com>
2020-11-07 14:25:33 -0500

    Pablo

--- Assets/Scripts/PrimerScript.cs
+++ Assets/Scripts/PrimerScript.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PrimerScript : MonoBehaviour
-{
+{   
+    Vector3 martinEsGay;
+
     // Start is called before the first frame update
     void Start()
     {
