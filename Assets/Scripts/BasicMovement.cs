a7f284094dc14795cef7b05adef98d1d581d200b
Martin <martinale4@hotmail.com>
2020-11-27 18:19:25 -0500

    script movimiento

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class BasicMovement : MonoBehaviour
 {
 
+    public Animator animacion;
+    public float velocidad;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,9 @@ public class BasicMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(x*Time.deltaTime*velocidad, 0, y*Time.deltaTime*velocidad));
     }
 }
