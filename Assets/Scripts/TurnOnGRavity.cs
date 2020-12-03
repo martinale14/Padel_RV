4753c618a9c4fbdee474ff0aabf33715207facc0
Martin <martinale4@hotmail.com>
2020-12-03 01:17:07 -0500

    Funcionalidades Kinect + impulso de la pelota

---
+++ Assets/Scripts/TurnOnGRavity.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnGRavity : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float impulse;
+    private int refe;
+    private float time;
+    public int factorGolp;
+    // Start is called before the first frame update
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+        refe = 0;
+        time = 0;
+        factorGolp = 1;
+    }
+
+    void Update(){
+
+        if(refe == 0){
+
+            refe = 1;
+            rb.AddForce((Vector3.up + Vector3.left)*impulse, ForceMode.Impulse);
+
+        }
+
+        time += Time.deltaTime;
+
+        if(time > 60){
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name.Equals("pala")){
+
+            Vector3 dir;
+            float colForce;
+
+            dir = collision.contacts[0].point - transform.position;
+            colForce = collision.impulse.magnitude;
+
+            dir = -dir.normalized;
+
+            rb.AddForce(dir*colForce*factorGolp, ForceMode.Impulse);
+
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/instanciarPelotas.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instanciarPelotas : MonoBehaviour
+{
+    private float tiempo;
+    public GameObject prefabBall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tiempo = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tiempo += Time.deltaTime;
+
+        if (tiempo >= 10){
+
+            Instantiate(prefabBall, new Vector3(4.98f,0.744f,-0.83f), Quaternion.identity);
+
+            tiempo = 0;
+        }
+    }
+}
