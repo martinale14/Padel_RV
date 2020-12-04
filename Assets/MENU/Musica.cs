96ba4f78fc1c015567f3808cc044cdb36b9c1bd8
valentina0110 <valentina.barajas@uao.edu.co>
2020-12-03 19:52:42 -0500

    Implementacion del menú principal

---
+++ Assets/MENU/Musica.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Musica : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Slider Volume;
+    public AudioSource myMusic;
+
+    // Update is called once per frame
+    void Update()
+    {
+        myMusic.volume = Volume.value;
+    }
+}
