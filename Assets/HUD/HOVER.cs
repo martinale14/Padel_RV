8229201926d844da9ebdadd8c5daa260b5d406b9
Juan Pablo <juanpari1766@gmail.com>
2020-12-04 04:32:02 -0500

    Se implementa el menú de pausa

--- Assets/HUD/HOVER.cs
+++ Assets/HUD/HOVER.cs
@@ -7,7 +7,7 @@ public class HOVER : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //time.timescale = 0 pausa = 1 play
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/pauseMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject menuPausaUI;
+    public GameObject HUDUI;
+    public static bool isPaused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void pause(){
+        menuPausaUI.SetActive(true);
+        HUDUI.SetActive(false);
+        Time.timeScale = 0f;
+    }
+
+    public void resume(){
+        menuPausaUI.SetActive(false);
+        HUDUI.SetActive(true);
+        Time.timeScale = 1f;
+    }
+
+}
