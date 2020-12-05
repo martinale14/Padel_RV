2f6028969b55d30fdab3700a056ff4810e559e0f
Martin <martinale4@hotmail.com>
2020-12-04 20:26:13 -0500

    Interface Sound and adding Colliders

--- Assets/MENU/Musica.cs
+++ Assets/MENU/Musica.cs
@@ -11,43 +11,20 @@ public class Musica : MonoBehaviour
     public Slider VolumeS;
     public AudioSource myMusic;
     public GameObject opciones;
-    private XmlDocument pref;
-    private XmlTextWriter writer;
-    private XmlNodeList xmlNode;
-    private float vol;
-    private FileStream fs;
+
 
     // Update is called once per frame
     void Awake()
     {
-        opciones = GameObject.Find("Opciones");
 
-        VolumeS = GameObject.Find("VolumenS").GetComponent<Slider>();
+        
 
-        pref = new XmlDocument();
-        fs = new FileStream("./Assets/MENU/pref.xml", FileMode.Open, FileAccess.ReadWrite);
-        pref.Load(fs);
-        xmlNode = pref.GetElementsByTagName("Volume");
-        print(float.Parse(xmlNode.Item(0).InnerText));
-
-       myMusic = GameObject.Find("Musiquita").GetComponent<AudioSource>();
-
-       vol = float.Parse(xmlNode.Item(0).InnerText);
-
-        VolumeS.value = vol;
-
-        fs.Close();
-
-        opciones.SetActive(false);
     }
 
     public void VolumeSli(){
          
-         fs = new FileStream("./Assets/MENU/pref.xml", FileMode.Open, FileAccess.ReadWrite);
-         xmlNode.Item(0).InnerText = VolumeS.value.ToString(); 
-         myMusic.volume = VolumeS.value;
-         pref.Save("./Assets/MENU/pref.xml");
-         fs.Close();
+         GameObject.Find("VolumeSource").GetComponent<AudioSource>().volume = GameObject.Find("VolumenS").GetComponent<Slider>().value;
+
     }
 
 }
