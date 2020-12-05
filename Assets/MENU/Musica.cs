3d37a4cbce37cf1ad701c2a81c56f2aa1f5b3332
Martin <martinale4@hotmail.com>
2020-12-04 19:26:16 -0500

    prefSystem

--- Assets/MENU/Musica.cs
+++ Assets/MENU/Musica.cs
@@ -10,30 +10,44 @@ public class Musica : MonoBehaviour
     // Start is called before the first frame update
     public Slider VolumeS;
     public AudioSource myMusic;
+    public GameObject opciones;
     private XmlDocument pref;
     private XmlTextWriter writer;
     private XmlNodeList xmlNode;
+    private float vol;
+    private FileStream fs;
 
     // Update is called once per frame
     void Awake()
     {
+        opciones = GameObject.Find("Opciones");
+
+        VolumeS = GameObject.Find("VolumenS").GetComponent<Slider>();
+
         pref = new XmlDocument();
-        if(File.Exists("./pref.xml")){
-            FileStream fs = new FileStream("pref.xml", FileMode.Open, FileAccess.Read);
-            pref.Load(fs);
-            xmlNode = pref.GetElementsByTagName("volume");
-            print(xmlNode);
-        }else{
-            pref.LoadXml("<sample><sample>");
-            writer = new XmlTextWriter("pref.xml", null);
-        }
+        fs = new FileStream("./Assets/MENU/pref.xml", FileMode.Open, FileAccess.ReadWrite);
+        pref.Load(fs);
+        xmlNode = pref.GetElementsByTagName("Volume");
+        print(float.Parse(xmlNode.Item(0).InnerText));
+
        myMusic = GameObject.Find("Musiquita").GetComponent<AudioSource>();
 
+       vol = float.Parse(xmlNode.Item(0).InnerText);
+
+        VolumeS.value = vol;
+
+        fs.Close();
+
+        opciones.SetActive(false);
     }
 
     public void VolumeSli(){
          
-         VolumeS = GameObject.Find("VolumenS").GetComponent<Slider>();
+         fs = new FileStream("./Assets/MENU/pref.xml", FileMode.Open, FileAccess.ReadWrite);
+         xmlNode.Item(0).InnerText = VolumeS.value.ToString(); 
          myMusic.volume = VolumeS.value;
+         pref.Save("./Assets/MENU/pref.xml");
+         fs.Close();
     }
+
 }
